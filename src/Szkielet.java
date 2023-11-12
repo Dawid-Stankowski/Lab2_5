cf86f9e01fd4516aab840a62074d9dab770814c4
Dawid-Stankowski <dawid441@interia.pl>
2023-11-12 21:54:45 +0100

    1.0

--- src/Szkielet.java
+++ src/Szkielet.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Szkielet {
+public class Szkielet {
+
+    String opisPierwszy;
+    String opisDrugi;
+    int opisTrzeci;
+    String opisCzwarty;
+
+    public Szkielet(String opisPierwszy, String opisDrugi, int opisTrzeci, String opisCzwarty) {
+        this.opisPierwszy = opisPierwszy;
+        this.opisDrugi = opisDrugi;
+        this.opisTrzeci = opisTrzeci;
+        this.opisCzwarty = opisCzwarty;
+    }
+
+    public void wyswietlInformacje() {
+        System.out.println("Inf 1: " + opisPierwszy);
+        System.out.println("Inf 2: " + opisDrugi);
+        System.out.println("Inf 3: " + opisTrzeci);
+        System.out.println("Inf 4: " + opisCzwarty);
+    }
+
+    public static void main(String[] args) {
+        Szkielet mojSzkielet = new Szkielet("Opis 1 ", "Opis 2", 1234, "Opis 4");
+
+                mojSzkielet.wyswietlInformacje();
+    }
 }
